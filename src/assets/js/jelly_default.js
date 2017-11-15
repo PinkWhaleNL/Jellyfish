1b2e58bd08158f9e33dd3540516af5f53e262208
Jasper Berkhout <jasper@mainpixel.io>
2017-11-15 08:34:58 +0100

    Added bootstrap-datepicker assets

--- src/assets/js/jelly_default.js
+++ src/assets/js/jelly_default.js
@@ -3,8 +3,12 @@ require('sweetalert');
 require('bootstrap-select');
 require('./bootstrap');
 
+// For optional modules.
+require('moment');
+require('./datepicker/bootstrap-datetimepicker.min');
+
 $('.selectpicker').selectpicker({
 	size: 4
 });
 
-
+$('.datepicker').datetimepicker({ format: 'Y-MM-DD' });
