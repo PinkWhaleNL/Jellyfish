844049e57702c5f7a2b153cdc99f730ac692acc3
Mr. J. del Luna <paperclamp@users.noreply.github.com>
2019-12-16 15:58:09 +0100

    Update AdminController.php

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -58,7 +58,7 @@ class AdminController extends Controller
         $type = ($id != 'new'?(new Types)->where('id', $id)->firstOrFail():(new Types));
         $type->sortable = (request()->sortable === 'true'? true: false);
         $type->publish_date = (request()->publish_date === 'true'? true: false);
-        $type->type = str_slug(request()->type);
+        $type->type = Str::slug(request()->type);
         $type->title = request()->title;
         $type->data = json_decode(request()->json);
         $type->save();
